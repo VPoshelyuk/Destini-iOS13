4ced43ee36fe73fcd71bcded85eb1913f7b62931
VPoshelyuk <aslava198@gmail.com>
2020-03-27 19:19:04 -0400

    MultipleChoice Qs Done

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -13,12 +13,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
+    var sB = StoryBrain()
+    @IBAction func choiceMade(_ sender: UIButton) {
+        sB.setCurrentStory(sender.currentTitle!)
+        updateUI()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
+    
+    func updateUI() {
+        storyLabel.text = sB.getText()
+        let choices = sB.getChoices()
+        choice1Button.setTitle(choices[0], for: .normal)
+        choice2Button.setTitle(choices[1], for: .normal)
+    }
 
 }
 
